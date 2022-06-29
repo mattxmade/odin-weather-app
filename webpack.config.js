056efed8b36e5270726f1c2a054f1418f956283e
Matt <97047080+mattxmade@users.noreply.github.com>
2022-06-29 13:39:15 +0100

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,42 @@
-const path = require('path');
-const Dotenv = require('dotenv-webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const Dotenv = require("dotenv-webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: './src/main.js', 
-  devtool: 'inline-source-map',
+  stats: {
+    errorDetails: true,
+  },
+  entry: "./src/main.js",
+  devtool: "inline-source-map",
   devServer: {
-    static: './dist',
+    static: "./dist",
   },
   plugins: [
     new HtmlWebpackPlugin({
       inject: true,
       title: "Weather App",
-      template: './src/template.html'
+      template: "./src/template.html",
     }),
     new Dotenv(),
-  ],  
+  ],
   output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
     assetModuleFilename: "[name][ext]",
     clean: true,
   },
   optimization: {
-    runtimeChunk: 'single',
+    runtimeChunk: "single",
   },
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        use: ["style-loader", "css-loader"],
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif|webp)$/i,
-        type: 'asset/resource',
+        type: "asset/resource",
       },
     ],
   },
