548848d1349427f9209d1abf541377ade3ab93d6
Matt <97047080+mattxmade@users.noreply.github.com>
2022-06-29 13:23:56 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,60 +1,61 @@
-import './style.css';
-import { getDate, isToday } from "date-fns";
+import "./style.css";
+import { isToday } from "date-fns";
 
 // NEED A H1 for SEO
 
-import weatherHeader from './header';
-import weatherTimeCard from './hour';
-import weatherDayCard from './week';
+import weatherHeader from "./header";
+import weatherTimeCard from "./hour";
+import weatherDayCard from "./week";
 
-const API_KEY = process.env.API_KEY
+const API_KEY = process.env.API_KEY;
 const url = `http://api.openweathermap.org/data/2.5/weather?q=London&APPID=${API_KEY}&units=metric`;
 const forecast = `http://api.openweathermap.org/data/2.5/forecast?q=London&APPID=${API_KEY}&units=metric`;
 
 const elements = {
   dashboards: {
-    overview: document.querySelector('.weather-overview'),
-    timeview: document.querySelector('.dashbaord-time'),
-    weekview: document.querySelector('.dashboard-week'),
+    overview: document.querySelector(".weather-overview"),
+    timeview: document.querySelector(".dashbaord-time"),
+    weekview: document.querySelector(".dashboard-week"),
     all: [
-      document.querySelector('.weather-overview'),
-      document.querySelector('.dashbaord-time'),
-      document.querySelector('.dashboard-week')
-    ]
-  }
-}
+      document.querySelector(".weather-overview"),
+      document.querySelector(".dashbaord-time"),
+      document.querySelector(".dashboard-week"),
+    ],
+  },
+};
 
 async function getForecast(url, forecast) {
   try {
-    const getWeather   = await fetch(url, {mode: 'cors'});
-    const getForecast  = await fetch(forecast, {mode: 'cors'});
-    
-    if (getWeather.status !== 200) return console.log(getWeather.status);       // https://stackoverflow.com/a/57503632
+    const getWeather = await fetch(url, { mode: "cors" });
+    const getForecast = await fetch(forecast, { mode: "cors" });
 
-    const weatherData  = await getWeather.json();
+    if (getWeather.status !== 200) return console.log(getWeather.status); // https://stackoverflow.com/a/57503632
+
+    const weatherData = await getWeather.json();
     const forecastData = await getForecast.json();
 
     let remove = [];
 
     for (const dashboard of elements.dashboards.all) {
-      dashboard.childNodes.forEach(node => remove.push(node));
+      dashboard.childNodes.forEach((node) => remove.push(node));
     }
 
     if (remove.length !== 0) {
-      remove.forEach(node => {
+      remove.forEach((node) => {
         node.remove();
       });
-      remove = [];     
+      remove = [];
     }
 
     weatherHeader(weatherData);
 
-    const ahead = processWeek(forecastData)
+    const ahead = processWeek(forecastData);
 
     weatherTimeCard(forecastData);
     weatherDayCard(ahead);
-    
-  } catch (error) { console.log(error) }
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 getForecast(url, forecast);
@@ -65,34 +66,33 @@ function processWeek(data) {
   array.push(data.list[0]);
 
   for (const item of data.list) {
-
     const date = new Date(item.dt_txt.slice(0, 10));
     const time = item.dt_txt.slice(11, 13);
 
-    if (!isToday(date) && time === '12') array.push(item);
+    if (!isToday(date) && time === "12") array.push(item);
   }
 
   return array;
 }
 
-const magicCurtain = document.querySelector('.magic-curtain');
-const clickables = document.querySelectorAll('.nav-icon');
-clickables.forEach(icon => icon.addEventListener('click', handleEvent));
+const magicCurtain = document.querySelector(".magic-curtain");
+const clickables = document.querySelectorAll(".nav-icon");
+clickables.forEach((icon) => icon.addEventListener("click", handleEvent));
 
 function handleEvent(e) {
-  magicCurtain.classList.add('curtain-call');
-  
+  magicCurtain.classList.add("curtain-call");
+
   // await data
   setTimeout(() => {
-    magicCurtain.classList.remove('curtain-call');
+    magicCurtain.classList.remove("curtain-call");
   }, 1600);
 }
 
-const inputSearch = document.querySelector('input');
-inputSearch.addEventListener('keypress', handleInput);
+const inputSearch = document.querySelector("input");
+inputSearch.addEventListener("keypress", handleInput);
 
 function handleInput(e) {
-  if (e.key === 'Enter' && e.target.value.trim().length !== 0) {
+  if (e.key === "Enter" && e.target.value.trim().length !== 0) {
     const city = e.target.value.toLowerCase().trim();
     //console.log(city);
 
